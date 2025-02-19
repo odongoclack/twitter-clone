767f3950c3f36c99154cf6f2a930212018c7d1f6
odongoclack <agerclackson44@gmail.com>
2025-02-19 18:48:49 +0300

    second commit

---
+++ components/Avatar.tsx
@@ -0,0 +1,15 @@
+interface AvatarProps {
+
+    userId: string;
+    isLarge?: boolean;
+    hasBorder?: boolean;
+}
+
+const Avatar: React.FC<AvatarProps>= () => {
+    return(
+        <div>
+
+        </div>
+    );
+}
+export default Avatar;
--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -9,21 +9,23 @@ export default async function handler(
   if (req.method !== "POST") {
     return res.status(405).end();
   }
+
   try {
     const { email, username, name, password } = req.body;
-    const hashePassword = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 12);
 
     const user = await prisma.user.create({
       data: {
         email,
         username,
         name,
-        hashePassword,
+        hashedPassword,
       },
     });
+
     return res.status(200).json(user);
   } catch (error) {
     console.log(error);
-    return res.status(400).end;
+    return res.status(400).end();
   }
 }
